2877b3e10a1266f09b27bf97f92a782f9bf5cb11
CortezFabrizio <65471614+CortezFabrizio@users.noreply.github.com>
2022-11-28 23:59:19 -0300

    Testing SQS with message from ECS task

--- FastAPIService/FastAPI_backend/test.py
+++ FastAPIService/FastAPI_backend/test.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+import requests
+import json
+import os
+import boto3
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class Ticker(BaseModel):
+    name: str
+
+
+@app.get("/get-ticker")
+def read_root(ticker:str):
+
+    sqs = boto3.resource('sqs')
+
+    print(ticker)
+    queue = sqs.get_queue_by_name(QueueName='TickerQueue')
+
+    response = queue.send_message(MessageBody=ticker)
+
+
+    return {"Hello": ticker}
