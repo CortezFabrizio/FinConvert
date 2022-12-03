18ae8df0c91d8029c685b0ef14d01fa45b93a9e6
CortezFabrizio <65471614+CortezFabrizio@users.noreply.github.com>
2022-12-02 23:53:39 -0300

    Identifying financial sections,Fixing headers sent to SEC endpoint,Optimizing code performance

--- lambdas/process_ticker_alternative.py
+++ lambdas/process_ticker_alternative.py
@@ -1,45 +1,87 @@
 import requests
 import json
-import bs4
+import time
+from bs4 import BeautifulSoup
 
-header_req = {"User-Agent":"fabriziocortez17@icloud.com","Accept-Encoding":"gzip,deflate","Host":"www.sec.gov"}
+sec_search_endpoint = 'https://efts.sec.gov/LATEST/search-index'
+
+header_req = {
+"user-Agent":"fabriziocortez17@gmail.com",
+"accept-Encoding":"gzip, deflate, br",
+"origin":"https://www.sec.gov",
+"content-type":"application/x-www-form-urlencoded; charset=UTF-8",
+"cache-control": "no-cache",
+"referer":"https://www.sec.gov/"
+}
+
 
 def search_cik(ticker):
     print(ticker)
 
-    url_index = 'https://efts.sec.gov/LATEST/search-index'
+    payload_d = json.dumps({"keysTyped":ticker})
 
-    payload = {"keysTyped":ticker}
-
-    req_index = requests.post(url=url_index,data=json.dumps(payload),headers=header_req)
+    req_index = requests.post(url=sec_search_endpoint,data=payload_d,headers=header_req)
 
     cik = req_index.json()["hits"]["hits"][0]["_id"]
+
     return cik
 
 
 def get_10_k_links(ticker):
 
     cik = search_cik(ticker)
-    
-    url_search = f'https://www.sec.gov/edgar/search/#/entityName={cik}&filter_forms=10-K'
 
-    req = requests.get(url_search).text
+    payload = json.dumps({"entityName":ticker,"filter_forms":"10-K","startdt":"2017-12-02","enddt":"2022-12-02"})
+
+    req = requests.post(sec_search_endpoint,headers=header_req,data=payload)
+
+    list_fillings = req.json()['hits']['hits']
+
+    list_adsh = [f'https://www.sec.gov/Archives/edgar/data/{cik}/'+filling['_source']['adsh'].replace('-','') for filling in list_fillings]
+
+    return list_adsh
 
 
-    parse_fillings = bs4.BeautifulSoup(req,features="html.parser").findAll("a",{"class":"preview-file"})
 
-    fillings_id = [f'https://www.sec.gov/Archives/edgar/data/{cik}/'+link['data-adsh'] for link in parse_fillings ]
+def get_statements(event,context):
 
-    return fillings_id
+    fillings = get_10_k_links(event['ticker'])
+
+    statements = {'income':[],'balance':[],'flows':[]}
+
+    for filling in fillings:
+
+        time.sleep(1)
+
+        for index in range(2,7):
+
+            time.sleep(1)
+
+            b = filling+f'/R{str(index)}.htm'
+
+            req = requests.get(b,headers=header_req).text
+
+            rows_list = BeautifulSoup(req,features="html.parser").find('table',{"class": "report"}).find_all('tr')
+
+            title = rows_list[0].find('th').getText().upper()
+            
+            if "OPERATION" in title or "INCOME" in title or "EARNING" in title:
+
+                statements['income'].append(filling)
+                continue
+
+            if "BALANCE" in title and "SHEET" in title:
+                statements['balance'].append(filling)
+
+                continue
+
+            if "CASH FLOW" in title:
+                statements['flows'].append(filling)
+
+                continue
 
 
-def get_fillings(event,context):
 
-    paths = get_10_k_links(event['ticker'])
-
-    for link in paths:
-
-        requests.get(link+'/R2.htm')
 
 
 
