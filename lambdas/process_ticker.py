b0a72d6f6fcf3448fee51d642937b45c4c5cb21c
CortezFabrizio <65471614+CortezFabrizio@users.noreply.github.com>
2023-01-13 23:20:46 -0300

    Update process_ticker.py

--- lambdas/process_ticker.py
+++ lambdas/process_ticker.py
@@ -2,13 +2,12 @@ import re
 import requests
 import json
 import time
-import boto3
 from bs4 import BeautifulSoup
 
 sec_search_endpoint = 'https://efts.sec.gov/LATEST/search-index'
 
 header_req = {
-"user-Agent":"fabriziocortez17@gmail.com",
+"user-Agent":"fabriziocortezandres@gmail.com",
 "accept-Encoding":"gzip, deflate, br",
 "origin":"https://www.sec.gov",
 "content-type":"application/x-www-form-urlencoded; charset=UTF-8",
@@ -33,36 +32,37 @@ def search_cik(ticker):
     return cik
 
 
-
 def date_validation(date):
 
     date_l = date.split('-')
-    dict_months = {1:'Jan',2:'Feb',3:'Mar',4:'Apr',5:'May',6:'Jun',7:'Jul',8:'Aug',9:'Sep',10:'Oct',11:'Nov',12:'Dec'} 
+    dict_months = {1:'Jan',2:'Feb',3:'Mar',4:'Apr',5:'May',6:'Jun',7:'Jul',8:'Aug',9:'Sep',10:'Oct',11:'Nov',12:'Dec'}
     date_valid = dict_months[int(date_l[1])]+f'.*{date_l[2]}.*{date_l[0]}'
     return {'regrex_expression':date_valid,'filling_year':date_l[0]}
 
 
-def get_10_k_links(ticker):
+def get_10_k_links(ticker,start_date,end_date):
 
     cik = search_cik(ticker)
 
-    payload = json.dumps({"entityName":cik,"filter_forms":"10-K","startdt":"2016-12-02","enddt":"2022-12-02"})
+    payload = json.dumps({"entityName":cik,"filter_forms":"10-K","startdt":f"{str(start_date)}-01-01","enddt":f"{str(end_date+1)}-12-31"})
 
     req = requests.post(sec_search_endpoint,headers=header_req,data=payload)
 
     list_fillings = req.json()['hits']['hits']
 
-    list_adsh = [{'date':date_validation(filling['_source']['period_ending']),'url':f'https://www.sec.gov/Archives/edgar/data/{cik}/'+filling['_source']['adsh'].replace('-','')} for filling in list_fillings if filling['_source']['file_type'] == '10-K' ]
+    list_adsh = [{'date':date_validation(filling['_source']['period_ending']),'url':f'https://www.sec.gov/Archives/edgar/data/{cik}/'+filling['_source']['adsh'].replace('-','')} for filling in list_fillings if filling['_source']['file_type'] == '10-K' and int(filling['_source']['period_ending'].split('-')[0]) <= end_date and int(filling['_source']['period_ending'].split('-')[0]) >= start_date ]
 
     return list_adsh
 
 
-def parse_statement(non_balance,file_url,date,year_filling,filling_dict):
+
+def parse_statement(non_balance,file_url,date,year_filling,filling_dict,end_date):
 
     req = requests.get(file_url,headers=header_req).text
 
     content = BeautifulSoup(req,features="html.parser")
     rows_list = content.find('table',{"class": "report"}).find_all('tr')
+
 
     if non_balance:
 
@@ -72,22 +72,24 @@ def parse_statement(non_balance,file_url,date,year_filling,filling_dict):
             if re.search(f'{date}',year.text,re.I):
                 filling_year = len(years)-(index+1)
 
-                valid_years = [ int(year_filling)-year_column for year_column in range(0,len(years[filling_year:])) if not int(year_filling)-year_column in filling_dict  ]
+                valid_years = [ int(year_filling)-year_column for year_column in range(0,len(years[filling_year:])) if not int(year_filling)-year_column in filling_dict and int(year_filling)-year_column >= end_date  ]
 
                 diff = len(years[filling_year:]) - len(valid_years)
- 
+
                 for year in valid_years:
-                    if not year in filling_dict:
-                        filling_dict[year] = {}
+                    filling_dict[year] = {}
 
                 break
 
+        concept_title = 'FirstBlock'
 
         for row in rows_list[2:]:
             cells = row.find_all('td',attrs={'class':re.compile('nump|num',re.I)})[filling_year+diff:]
 
             if cells:
-    
+
+                concept = row.find('td',attrs={'class':'pl'}).getText()
+       
 
                 for index,cell in enumerate(cells):
 
@@ -99,20 +101,34 @@ def parse_statement(non_balance,file_url,date,year_filling,filling_dict):
                     if cell_class == 'num':
                         non_digit_filter = f'({non_digit_filter})'
 
-                    filling_dict[year_key][concept] = non_digit_filter
+                    try:
+                        filling_dict[year_key][concept_title][concept] = non_digit_filter
+                    except:
+                        filling_dict[year_key][concept_title] = {concept:non_digit_filter}
+
+                  
+
+            else:
+                try:
+                    concept_title = row.find('td',attrs={'class':'pl'}).getText()
+                except:
+                    continue
+
 
 
 def get_statements(event,context):
 
+    start_date = event['start_date']
+    end_date = event['end_date']
+
     statements = {'Balance':{},'Cash':{},'Income':{}}
 
-    fillings = get_10_k_links(event['ticker'])
+    fillings = get_10_k_links(event['ticker'],start_date,end_date)
 
     for filling_dict in fillings:
         filling = filling_dict['url']
         filling_date = filling_dict['date']['regrex_expression']
         filling_year = filling_dict['date']['filling_year']
-        print(filling)
        
         #time.sleep(1)
         filling_summary = filling+'/FilingSummary.xml'
@@ -128,44 +144,20 @@ def get_statements(event,context):
             report_name = report.find('ShortName').getText().upper()
             html_file_name = report.find('HtmlFileName').getText()
             file_url = filling+f'/{html_file_name}'
-            print(file_url)
-            print('------=============================------------')
-
+         
             if "OPERATION" in report_name or "INCOME" in report_name or "EARNING" in report_name:
                 if not income_state:
-                    #parse_statement(True,file_url,filling_date,filling_year,statements)
+                    parse_statement(True,file_url,filling_date,filling_year,statements['Income'],start_date)
+
                     income_state = True
 
             if "BALANCE" in report_name and "SHEET" in report_name:
                 if not balance_state:
-                    parse_statement(False,file_url,filling_date,filling_year,statements['Balance'])
+                    parse_statement(False,file_url,filling_date,filling_year,statements['Balance'],start_date)
                     balance_state = True
 
             if "CASH FLOW" in report_name:
                 if not flow_state:
-                    parse_statement(True,file_url,filling_date,filling_year,statements['Cash'])
+                    parse_statement(True,file_url,filling_date,filling_year,statements['Cash'],start_date)
                     flow_state = True
-                    
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('fabri_app')
-    table.put_item(
-
-            Item={'ticker':event['ticker'],
-                   'financial_concepts':filling_concepts
-            
-            }
-
-
-    )
-
-
-
-
-
-        
-
-
-
-
-
 
