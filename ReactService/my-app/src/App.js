358c32bea549e74a6df362c5e24d32acbfed91a2
CortezFabrizio <65471614+CortezFabrizio@users.noreply.github.com>
2023-02-01 23:35:06 -0300

    React function sending to fastAPI backend locally

--- ReactService/my-app/src/App.js
+++ ReactService/my-app/src/App.js
@@ -4,26 +4,30 @@ import React, { Component } from 'react';
 import  ReactDOM  from 'react-dom';
 
 
+function send_api() {
+  const ticker_value = document.getElementById("ticker").value;
+  const start_date = document.getElementById("end_date").value;
+  const end_date = document.getElementById("start_date").value;
 
-function f() {
-  const ge = document.getElementById("oi").value;
+  const url = new URL ('http://0.0.0.0:80/get-ticker')
   
-  const url = null
-  
-  const po = {ticker:ge}
+  const params = {'ticker':ticker_value,'start_date':start_date,'end_date':end_date}
+  url.search = new URLSearchParams(params).toString();
+
 
   const info = {
-    method:"POST",
-    body: JSON.stringify(po),
-    headers: {"Content-Type":"application/json"}
+    method:"GET",
+    header:{'Access-Control-Allow-Origin':'*'}
   }
 
-  const yy = fetch(url,info).then(function DD(response){
-    const res = response.json()
-    console.log(res)
-    return res
-    
-   });
+  const req = fetch(url,info).then(function DD(response){
+    //const res = response.json()
+
+    console.log(response.json() ,typeof(response))
+    return response
+
+
+   })   ;
 
 }
 
@@ -31,11 +35,16 @@ function App() {
   return (
     <div id="lolo" className="App">
       <p>Hola Mundo</p>
-      <input id='oi' type="text"></input>
-      <input type={"button"} value="Search" onClick={f} ></input>
+
+      <input id='ticker' type="text"></input>
+      <input id='end_date' type="text"></input>
+      <input id='start_date' type="text"></input>
+
+      <input type={"button"} value="Search" onClick={send_api} ></input>
       
     </div>
   );
 }
 
 export default App;
+
