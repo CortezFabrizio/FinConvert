58adfa225fb94a746907b4d0b13a43bc498219b0
CortezFabrizio <65471614+CortezFabrizio@users.noreply.github.com>
2023-02-21 23:20:00 -0300

    Displaying title and statement's currency, and more readability

--- FastAPIService/FastAPI_backend/excel_creator.py
+++ FastAPIService/FastAPI_backend/excel_creator.py
@@ -22,6 +22,10 @@ def excel_creator(ticker:str,start_date:int,end_date:int):
         wb = Workbook()
         sheet = wb.worksheets[0]
 
+        sheet.column_dimensions['E'].width = 25
+        sheet.column_dimensions['P'].width = 25
+        sheet.column_dimensions['Y'].width = 25
+
         row_index = 1
 
         for year in financials:
@@ -38,33 +42,39 @@ def excel_creator(ticker:str,start_date:int,end_date:int):
                 if statement_name == 'Income':
                     column_index = 3
                 elif statement_name == 'Balance':
-                    column_index = 7
+                    column_index = 14
                 elif statement_name == 'Cash':
-                    column_index = 11
+                    column_index = 23
 
                 statement = statements[statement_name]
 
-                sheet.cell(row=original_row_index,column=column_index,value=statement_name)
+                title = statement['title']
 
+                sheet.cell(row=original_row_index+1,column=column_index,value=title)
+
+                del statement['title']
 
                 for concept_title in statement:
-                    
-                    sheet.cell(row=original_row_index,column=column_index+1,value=concept_title)
+
+                    sheet.cell(row=original_row_index+3,column=column_index+1,value=concept_title)
 
                     concepts = statement[concept_title]
 
                     for concept in concepts:
                         value = concepts[concept]
-                        sheet.cell(row=original_row_index,column=column_index+2,value=concept)
-                        sheet.cell(row=original_row_index,column=column_index+3,value=value)
+
+                        sheet.cell(row=original_row_index+4,column=column_index+2,value=concept)
+                        sheet.cell(row=original_row_index+4,column=column_index+3,value=value)
                         original_row_index+=1
+
+                    original_row_index+=2
 
                 difference_by_length = original_row_index-row_index
                 if difference_by_length > greater_length:
                     greater_length = difference_by_length
 
             else:
-                row_index+= 5+greater_length
+                row_index+= greater_length+4
 
 
         with NamedTemporaryFile() as tmp:
