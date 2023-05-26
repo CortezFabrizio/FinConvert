aac9f49e008061b164bb49c39e53273e9ef4a059
CortezFabrizio <65471614+CortezFabrizio@users.noreply.github.com>
2023-05-26 00:02:21 -0300

    Putting in a separate module excel creation function

---
+++ FastAPIService/excel_creator.py
@@ -0,0 +1,79 @@
+import json
+import requests
+
+from tempfile import NamedTemporaryFile
+from openpyxl import Workbook
+
+
+def create_excel (ticker,start_date,end_date):
+
+        param = {'ticker':ticker,'start_date':start_date,'end_date':end_date}
+
+        res = requests.get('http://127.0.0.1:8000/get-ticker',param)
+
+        financials = json.loads(res.json())
+
+        wb = Workbook()
+        sheet = wb.worksheets[0]
+
+        sheet.column_dimensions['E'].width = 25
+        sheet.column_dimensions['P'].width = 25
+        sheet.column_dimensions['Y'].width = 25
+
+        row_index = 1
+
+        for year in financials:
+
+            statements = financials[year]
+
+            sheet.cell(row=row_index,column=1 ,value=year)
+
+            greater_length = 0
+
+            for statement_name in statements:
+                original_row_index = row_index
+
+                if statement_name == 'Income':
+                    column_index = 3
+                elif statement_name == 'Balance':
+                    column_index = 14
+                elif statement_name == 'Cash':
+                    column_index = 23
+
+                statement = statements[statement_name]
+
+                title = statement['title']
+
+                sheet.cell(row=original_row_index+1,column=column_index,value=title)
+
+                del statement['title']
+
+                for concept_title in statement:
+
+                    sheet.cell(row=original_row_index+3,column=column_index+1,value=concept_title)
+
+                    concepts = statement[concept_title]
+
+                    for concept in concepts:
+                        value = concepts[concept]
+
+                        sheet.cell(row=original_row_index+4,column=column_index+2,value=concept)
+                        sheet.cell(row=original_row_index+4,column=column_index+3,value=value)
+                        original_row_index+=1
+
+                    original_row_index+=2
+
+                difference_by_length = original_row_index-row_index
+                if difference_by_length > greater_length:
+                    greater_length = difference_by_length
+
+            else:
+                row_index+= greater_length+4
+
+
+        with NamedTemporaryFile() as tmp:
+                file_path = tmp.name
+                wb.save(file_path)
+                stream = tmp.read()
+
+        return stream
